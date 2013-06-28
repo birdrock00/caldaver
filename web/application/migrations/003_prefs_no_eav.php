birdrock00/caldaver

commit 7c97f11ed6f81ee30ea8655630295a0f7ba9fee3

Fix dbupgrade when dbprefix is set (closes #66)

web/application/migrations/003_prefs_no_eav.php
@@ -6,9 +6,10 @@ class Migration_Prefs_no_eav extends CI_Migration {
         echo "Creating table prefs...\n";
 
         // DBForge doesn't support foreign keys
+        $prefs_table = $this->db->protect_identifiers('prefs', true);
         if (preg_match('/^mysql/', $this->db->dbdriver)) {
             $sql = <<< 'MYSQL'
-                CREATE TABLE prefs (
+                CREATE TABLE $prefs_table (
                         username VARCHAR(255) NOT NULL,
                         options TEXT NOT NULL,
                         PRIMARY KEY(username)
@@ -16,7 +17,7 @@ class Migration_Prefs_no_eav extends CI_Migration {
 MYSQL;
         } elseif ($this->db->dbdriver == 'postgre') {
             $sql = <<< 'PGSQL'
-                CREATE TABLE prefs (
+                CREATE TABLE $prefs_table (
                         username varchar(255) not null,
                         options text not null,
                         primary key (username));

web/application/migrations/001_share_write_access.php
@@ -3,12 +3,14 @@
 class Migration_Share_write_access extends CI_Migration {
     public function up() {
         echo "Adding column write_access to shared table...\n";
+
+        $shared_table = $this->db->protect_identifiers('shared', true);
         
         // DBForge PostgreSQL support is broken
         // (https://github.com/EllisLab/CodeIgniter/issues/808)
         // Can't use it to add a new column :-(
-        $this->db->query("ALTER TABLE shared ADD COLUMN write_access BOOLEAN
-                NOT NULL DEFAULT '0'");
+        $this->db->query("ALTER TABLE ".$shared_table
+            ." ADD COLUMN write_access BOOLEAN NOT NULL DEFAULT '0'");
 
         // AgenDAV only provided write access prior to version 1.2.5
         echo "Setting initial value for write_access...\n";

commit 121572c373c65ecae9cb4976e0de01eb9a112fdd

Fix error, introduced in 7c97f11ed6f81ee30ea8655630295a0f7ba9fee3, that leads to miss-named table-names

web/application/migrations/003_prefs_no_eav.php
@@ -8,7 +8,7 @@ class Migration_Prefs_no_eav extends CI_Migration {
         // DBForge doesn't support foreign keys
         $prefs_table = $this->db->protect_identifiers('prefs', true);
         if (preg_match('/^mysql/', $this->db->dbdriver)) {
-            $sql = <<< 'MYSQL'
+            $sql = <<< MYSQL
                 CREATE TABLE $prefs_table (
                         username VARCHAR(255) NOT NULL,
                         options TEXT NOT NULL,
@@ -16,7 +16,7 @@ class Migration_Prefs_no_eav extends CI_Migration {
                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 MYSQL;
         } elseif ($this->db->dbdriver == 'postgre') {
-            $sql = <<< 'PGSQL'
+            $sql = <<< PGSQL
                 CREATE TABLE $prefs_table (
                         username varchar(255) not null,
                         options text not null,